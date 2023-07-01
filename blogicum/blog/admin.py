6dba5690da58a58417159f291a60ddd39e4e43d7
Евгений Марчукевич <evgenymarchukevitch@yandex.ru>
2023-07-01 19:06:16 +0300

    flake8

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -12,33 +12,25 @@ class LocationAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = (
-                    'title',
+    list_display = ('title',
                     'is_published',
                     'category',
                     'location',
                     'pub_date',
                     'author',
-                    'text'
-    )
-    list_editable = (
-                     'location',
+                    'text')
+    list_editable = ('location',
                      'category',
-                     'is_published'
-    )
-    search_fields = (
-                     'title',
+                     'is_published')
+    search_fields = ('title',
                      'category',
                      'is_published',
                      'location',
                      'author',
-                     'text'
-    )
-    list_filter = (
-                   'category',
+                     'text')
+    list_filter = ('category',
                    'location',
-                   'is_published',
-                   )
+                   'is_published')
 
 
 admin.site.register(Post, PostAdmin)
